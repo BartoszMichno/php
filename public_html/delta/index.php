b4a0096db5b0a519e07ea9c176de71d0a9255e7b
2E-2 <2e-2@s42s07mac.local>
2019-05-16 10:03:19 +0200

    1

---
+++ public_html/delta/index.php
@@ -0,0 +1,41 @@
+<?php
+// Włączone pełne raportowanie błędów
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+
+// sprawdza istnienie
+if(isset($_GET['a']) and isset($_GET['b']) and isset($_GET['c'])){
+   
+   if(is_numeric($_GET['a']) and is_numeric($_GET['b']) and is_numeric($_GET['c'])){
+      echo'oblicz delte';
+   
+   $a=$_GET['a'];
+   $a=$_GET['b'];
+   $a=$_GET['c'];
+
+   $delta = $b*$b - 4*$a*$c;
+
+   if($delta>0){
+      $x1= (-$b - sqrt($delta))/2*$a;
+      $x2= (-$b + sqrt($delta))/2*$a;
+
+      include('views/d-greater.php');
+   }
+   elseif($delta==0){
+      $x=-$b/2*$a;
+      include('views/d-equal.php');
+   }
+   else{
+      include('views/d-less.php');
+   }
+}
+else{
+   include('views/error.php');
+}
+}
+
+
+
+else{
+   include('views/form.php');
+}
---
+++ public_html/delta/views/d-greater.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+</head>
+<body>
+   <h1>oblicznie delty</h1>
+   <p>parametry równania:</p>
+   <ul>
+   <li><?php echo $a ?></li>
+   <li><?php echo $b ?></li>
+   <li><?php echo $c ?></li>
+   </ul>
+</body>
+</html>
